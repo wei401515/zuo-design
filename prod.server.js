6bd671890e41eb0cbe57ea9bf06098dabe02db1d
dllo <dllo@weishibodeMac-mini.local>
2017-12-08 17:48:52 +0800

    webpack

--- prod.server.js
+++ prod.server.js
@@ -1,19 +1,27 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 const proxy = require('http-proxy-middleware')
-//
-const config = require('./config')
-const app = express()
+const config = require('./config');
+const app = express();
+const http = require('http')
+const https = require('https')
+const fs = require('fs')
+
+let options = {
+  key: fs.readFileSync('./2_www.weishibo401515.cn.key'),
+  cert: fs.readFileSync('./1_www.weishibo401515.cn_bundle.crt')
+}
 
 
-app.use(express.static(path.join(__dirname,'dist')))
-console.log(config.dev.proxyTable)
-// app.use(key,proxy(value))
-let proxyTable = config.dev.proxyTable
+let proxyTable = config.dev.proxyTable;
+
 Object.keys(proxyTable).forEach(function (key,index,array) {
-  app.use(key, proxy(proxyTable[key]))
+  app.use(key,proxy(proxyTable[key]))
+})
+// console.log(config.dev.proxyTable);
+app.use(express.static(path.join(__dirname,'dist')))
+http.createServer(app).listen(80,() => {
+  console.log('server is running in http://localhost:80');
 })
 
-app.listen(8080, () => {
-  console.log('server is running in http//localhost:8080')
-})
+https.createServer(options,app).listen(443);
